07290b426fd6d83fe32ff3cefce18fc04a97b056
amit-rgs <amit.patidar@rgs-tech.com>
2026-07-29 17:02:49 +0530

    Add outputs for GitHub Actions and runtime service accounts

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,11 @@
 output "artifact_registry_url" {
-
   value = module.artifact_registry.repository_url
+}
 
+output "github_actions_service_account" {
+  value = module.service_accounts.github_service_account_email
+}
+
+output "runtime_service_account" {
+  value = module.service_accounts.runtime_service_account_email
 }
