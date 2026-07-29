268b6ef554279e31054b23892ea291ea05ee3cff
amit-rgs <amit.patidar@rgs-tech.com>
2026-07-29 17:22:03 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "github_actions_service_account" {
 output "runtime_service_account" {
   value = module.service_accounts.runtime_service_account_email
 }
+
+output "cloud_run_url" {
+  value = module.cloud_run.service_url
+}
+
+output "cloud_run_name" {
+  value = module.cloud_run.service_name
+}
