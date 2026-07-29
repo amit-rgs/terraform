f6b90771d9256fc9650f272f486f565c634643c9
amit-rgs <amit.patidar@rgs-tech.com>
2026-07-29 17:07:05 +0530

    Add IAM roles for GitHub service account

---
+++ modules/iam/main.tf
@@ -0,0 +1,23 @@
+locals {
+
+  github_roles = [
+
+    "roles/run.admin",
+    "roles/artifactregistry.admin",
+    "roles/storage.admin",
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/iam.serviceAccountUser",
+    "roles/iam.serviceAccountAdmin",
+    "roles/logging.viewer",
+    "roles/monitoring.viewer",
+    "roles/browser"
+  ]
+}
+
+resource "google_project_iam_member" "github_roles" {
+
+  for_each = toset(local.github_roles)
+  project = var.project_id
+  role = each.value
+  member = "serviceAccount:${var.github_service_account_email}"
+}
