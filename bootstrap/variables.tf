ea284825dd4e6efc93860fe18b0642486adae9e7
amit-rgs <amit.patidar@rgs-tech.com>
2026-07-30 15:39:42 +0530

    Update variables.tf

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,12 +1,63 @@
+#############################################
+# GCP Project Configuration
+#############################################
+
 variable "project_id" {
-  type = string
+  description = "Google Cloud Project ID"
+  type        = string
 }
 
 variable "region" {
-  type    = string
-  default = "asia-south1"
+  description = "Google Cloud Region"
+  type        = string
+  default     = "asia-south1"
 }
 
-variable "bucket_name" {
-  type = string
+#############################################
+# Terraform State Bucket
+#############################################
+
+variable "terraform_state_bucket_name" {
+  description = "Terraform Remote State Bucket Name"
+  type        = string
+}
+
+#############################################
+# GitHub Repository
+#############################################
+
+variable "github_owner" {
+  description = "GitHub Organization or Username"
+  type        = string
+}
+
+variable "github_repository" {
+  description = "GitHub Repository Name"
+  type        = string
+}
+
+#############################################
+# Service Account
+#############################################
+
+variable "github_service_account_name" {
+  description = "GitHub Actions Service Account Name"
+  type        = string
+  default     = "github-actions-sa"
+}
+
+#############################################
+# Workload Identity
+#############################################
+
+variable "workload_identity_pool_id" {
+  description = "Workload Identity Pool ID"
+  type        = string
+  default     = "github-pool"
+}
+
+variable "workload_identity_provider_id" {
+  description = "Workload Identity Provider ID"
+  type        = string
+  default     = "github-provider"
 }
